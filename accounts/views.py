4013835962126fe6b04a256a6c5a8c43bef05e3b
Dipto-Datta <diptodattag@gmail.com>
2022-11-11 02:12:53 +0600

    login error

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
@@ -13,3 +13,12 @@ def signup_view(request):
         form = UserCreationForm()
     return render (request,'accounts/signup.html',{'form':form})
 
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            form.get_user()
+            return redirect('articles:list')
+    else:
+        form = AuthenticationForm()
+    return render (request,'accounts/login.html',{'form': form})
